49eaec8863a447307a866b45b958f48af19098cf
VentsislavKostadinov <ventsislav.kostadinov@angloamerican.com>
2023-12-21 16:13:31 +0200

    render dynamic meal details

--- app/meals/[mealSlug]/page.tsx
+++ app/meals/[mealSlug]/page.tsx
@@ -1,3 +1,37 @@
-export default function MealDetailsPage() {
-    return <h1>Meal Details</h1>
+import classes from './page.module.scss'
+import Image from 'next/image'
+import { getMeal } from '@/lib/meals'
+import { MealSlugProps, MealProps } from '@/app/model/meal-slug'
+
+export const MealDetailsPage = ({ params }: MealSlugProps) => {
+    const meal = getMeal(params.mealSlug) as MealProps
+
+    const { image, title, creator, creator_email, summary, instructions } = meal
+    const listInstructions = instructions.split(/\n/g)
+    return (
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={image} alt={title} fill />
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{title}</h1>
+                    <p className={classes.creator}>
+                        by <a href={`mailto:${creator_email}`}>{creator}</a>
+                    </p>
+                    <p className={classes.summary}>{summary}</p>
+                </div>
+            </header>
+            <main>
+                <ul className={classes.instructions}>
+                    {listInstructions.map(
+                        (instruction: string) =>
+                            instruction && <li>{instruction}</li>,
+                    )}
+                </ul>
+            </main>
+        </>
+    )
 }
+
+export default MealDetailsPage
