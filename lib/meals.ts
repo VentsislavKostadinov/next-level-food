25941ec232e52dc4ffa85ca313e92cf55819fb26
VentsislavKostadinov <ventsislav.kostadinov@angloamerican.com>
2023-12-24 11:36:01 +0200

    add some logs

--- lib/meals.ts
+++ lib/meals.ts
@@ -22,36 +22,40 @@ export function getMeal(slug: string) {
     return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug)
 }
 
-export async function saveMeal(meal: any) {
+export const saveMeal = async (meal: any) => {
     meal.slug = slugify(meal.title, { lower: true })
 
     const extension = meal.image.name.split('.').pop()
     const fileName = `${meal.slug}.${extension}`
 
-    const bufferedImage = await meal.image.arrayBuffer()
+    try {
+        const bufferedImage = await meal.image.arrayBuffer()
 
-    s3.putObject({
-        Bucket: 'ventsislav-kostadinov-next-food-level',
-        Key: fileName,
-        Body: Buffer.from(bufferedImage),
-        ContentType: meal.image.type,
-    })
+        s3.putObject({
+            Bucket: 'ventsislav-kostadinov-next-food-level',
+            Key: fileName,
+            Body: Buffer.from(bufferedImage),
+            ContentType: meal.image.type,
+        })
 
-    meal.image = fileName
+        meal.image = fileName
 
-    db.prepare(
-        `
-    INSERT INTO meals
-      (title, summary, instructions, creator, creator_email, image, slug)
-    VALUES (
-      @title,
-      @summary,
-      @instructions,
-      @creator,
-      @creator_email,
-      @image,
-      @slug
-    )
-  `,
-    ).run(meal)
+        db.prepare(
+            `
+        INSERT INTO meals
+          (title, summary, instructions, creator, creator_email, image, slug)
+        VALUES (
+          @title,
+          @summary,
+          @instructions,
+          @creator,
+          @creator_email,
+          @image,
+          @slug
+        )
+      `,
+        ).run(meal)
+    } catch (e: any) {
+        console.log(e)
+    }
 }
