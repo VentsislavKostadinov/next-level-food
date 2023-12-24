67de52a8bcc0a75f54982c8d4ca44d63b93cb8cb
VentsislavKostadinov <ventsislav.kostadinov@angloamerican.com>
2023-12-24 11:19:22 +0200

    add try/catch block in actions.ts

--- lib/actions.ts
+++ lib/actions.ts
@@ -10,30 +10,34 @@ const isInvalidText = (text: FormDataEntryValue | null) => {
 }
 
 export const shareMeal = async (prevState: FormData, formData: FormData) => {
-    const meal = {
-        title: formData.get('title'),
-        summary: formData.get('summary'),
-        instructions: formData.get('instructions'),
-        image: formData.get('image'),
-        creator: formData.get('name'),
-        creator_email: formData.get('email'),
-    }
-
-    if (
-        isInvalidText(meal.title) ||
-        isInvalidText(meal.summary) ||
-        isInvalidText(meal.instructions) ||
-        isInvalidText(meal.creator) ||
-        isInvalidText(meal.creator_email) ||
-        //@ts-ignore
-        !meal.creator_email.includes('@') ||
-        !meal.image
-    ) {
-        return {
-            message: 'Invalid Input!',
+    try {
+        const meal = {
+            title: formData.get('title'),
+            summary: formData.get('summary'),
+            instructions: formData.get('instructions'),
+            image: formData.get('image'),
+            creator: formData.get('name'),
+            creator_email: formData.get('email'),
         }
+
+        if (
+            isInvalidText(meal.title) ||
+            isInvalidText(meal.summary) ||
+            isInvalidText(meal.instructions) ||
+            isInvalidText(meal.creator) ||
+            isInvalidText(meal.creator_email) ||
+            //@ts-ignore
+            !meal.creator_email.includes('@') ||
+            !meal.image
+        ) {
+            return {
+                message: 'Invalid Input!',
+            }
+        }
+        await saveMeal(meal)
+        revalidatePath('/meals')
+        redirect('/meals')
+    } catch (e: any) {
+        console.log('Failed to fetch data', e)
     }
-    await saveMeal(meal)
-    revalidatePath('/meals')
-    redirect('/meals')
 }
